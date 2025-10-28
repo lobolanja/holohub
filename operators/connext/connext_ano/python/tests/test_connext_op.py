483a6da96ad793f732a1f496756394e75d2392b1
irene <irene@rti.com>
2025-10-28 14:38:14 +0100

    test timeout increased. uncommented the rest of tests

--- operators/connext/connext_ano/python/tests/test_connext_op.py
+++ operators/connext/connext_ano/python/tests/test_connext_op.py
@@ -195,73 +195,73 @@ class ConnextApplicationDDSDummy(Application):
         self.add_flow(self._rx_op, self._sink, {("output", "input")})
         sleep(2)
 
-#
-# def test_tx_constructs_with_defaults():
-#     app = Application()
-#     op = ConnextAnoTxOp(fragment=app)
-#     assert op is not None
-#
-#
-# def test_rx_constructs_with_defaults():
-#     app = Application()
-#     op = ConnextAnoRxOp(fragment=app)
-#     assert op is not None
-#
-#
-# def test_tx_starts_and_stops():
-#     app = Application()
-#     tx = ConnextAnoTxOp(
-#         fragment=app,
-#         ano_config=ANOConfig(enabled=True),
-#         dds_config=DDSConfig(),
-#     )
-#     tx.start()
-#     tx.stop()
-#     assert True  # If no exceptions, the test passes
-#
-#
-# def test_rx_starts_and_stops():
-#     app = Application()
-#     rx = ConnextAnoRxOp(
-#         fragment=app,
-#         ano_config=ANOConfig(enabled=True),
-#         dds_config=DDSConfig(),
-#     )
-#     rx.start()
-#     rx.stop()
-#     assert True  # If no exceptions, the test passes
-#
-# def test_tx_rx_discovery():
-#
-#     rx_shm_name = "rx_shm_memory"
-#
-#     app = Application()
-#     tx = ConnextAnoTxOp(
-#         fragment=app,
-#         ano_config=ANOConfig(shm_name="tx_shm_memory", enabled=True),
-#         dds_config=DDSConfig(),
-#     )
-#     rx = ConnextAnoRxOp(
-#         fragment=app,
-#         ano_config=ANOConfig(shm_name=rx_shm_name, enabled=True),
-#         dds_config=DDSConfig(),
-#     )
-#
-#     tx.start()
-#     rx.start()
-#     sleep(2)  # Allow some time for discovery to complete
-#
-#     # Check if both operators have finished the discovery process
-#     assert rx_shm_name in tx.connext_ano_writer.get_discovery_manager().get_destinations().values()
-#     tx.stop()
-#     rx.stop()
-#     sleep(2)
-#     assert True
+
+def test_tx_constructs_with_defaults():
+    app = Application()
+    op = ConnextAnoTxOp(fragment=app)
+    assert op is not None
+
+
+def test_rx_constructs_with_defaults():
+    app = Application()
+    op = ConnextAnoRxOp(fragment=app)
+    assert op is not None
+
+
+def test_tx_starts_and_stops():
+    app = Application()
+    tx = ConnextAnoTxOp(
+        fragment=app,
+        ano_config=ANOConfig(enabled=True),
+        dds_config=DDSConfig(),
+    )
+    tx.start()
+    tx.stop()
+    assert True  # If no exceptions, the test passes
+
+
+def test_rx_starts_and_stops():
+    app = Application()
+    rx = ConnextAnoRxOp(
+        fragment=app,
+        ano_config=ANOConfig(enabled=True),
+        dds_config=DDSConfig(),
+    )
+    rx.start()
+    rx.stop()
+    assert True  # If no exceptions, the test passes
+
+def test_tx_rx_discovery():
+
+    rx_shm_name = "rx_shm_memory"
+
+    app = Application()
+    tx = ConnextAnoTxOp(
+        fragment=app,
+        ano_config=ANOConfig(shm_name="tx_shm_memory", enabled=True),
+        dds_config=DDSConfig(),
+    )
+    rx = ConnextAnoRxOp(
+        fragment=app,
+        ano_config=ANOConfig(shm_name=rx_shm_name, enabled=True),
+        dds_config=DDSConfig(),
+    )
+
+    tx.start()
+    rx.start()
+    sleep(2)  # Allow some time for discovery to complete
+
+    # Check if both operators have finished the discovery process
+    assert rx_shm_name in tx.connext_ano_writer.get_discovery_manager().get_destinations().values()
+    tx.stop()
+    rx.stop()
+    sleep(2)
+    assert True
 
 def test_tx_rx_ano_integration():
     rx_shm_name = "rx_shm_memory"
     payload = "hello_holoscan"
-    timeout = 15 # seconds
+    timeout = 25 # seconds
 
     app_tx = ConnextApplicationANODummy(rx_shm_name=rx_shm_name, payload=payload)
 
